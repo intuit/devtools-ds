1929243e6414ada36f9acb06b4c6d55e6b798693
Tyler Krupicka <tylerkrupicka@gmail.com>
2021-02-01 11:46:22 -0800

    improve create script, make bin

--- packages/create/src/index.tsx
+++ packages/create/src/index.tsx
@@ -1,5 +1,6 @@
 import { prompt } from "enquirer";
 import signale from "signale";
+import { execSync } from "child_process";
 import { v4 as uuid } from "uuid";
 import { paramCase, constantCase, capitalCase } from "change-case";
 import copy from "copy-template-dir";
@@ -21,60 +22,96 @@ const getTemplatePath = (template: string) => {
   return join(__dirname, "../../templates/", template);
 };
 
+/** Get the default author name from the local system. */
+const defaultAuthorName = () => {
+  try {
+    return String(execSync("git config --get user.name")).trim();
+  } catch (error) {
+    return "";
+  }
+};
+
+/** Get the default author email from the local system. */
+const defaultAuthorEmail = () => {
+  try {
+    return String(execSync("git config --get user.email")).trim();
+  } catch (error) {
+    return "";
+  }
+};
+
 /** Build a template */
 const main = async () => {
   signale.start("Starting devtools-ds project generation..");
-  let responses = (await prompt([
-    {
-      type: "select",
-      name: "template",
-      message: "What template would you like to use?",
-      choices: getTemplates(),
-    },
-    {
-      type: "input",
-      name: "projectName",
-      message: "What is your devtools project's name?",
-    },
-    {
-      type: "input",
-      name: "projectOrg",
-      message: "What will your project's GitHub organization be?",
-    },
-    { type: "input", name: "devName", message: "What is your name?" },
-    { type: "input", name: "devEmail", message: "What is your email?" },
-  ])) as any;
+  const defaultName = defaultAuthorName();
+  const defaultEmail = defaultAuthorEmail();
+  try {
+    let responses = (await prompt([
+      {
+        type: "select",
+        name: "template",
+        message: "What template would you like to use?",
+        choices: getTemplates(),
+      },
+      {
+        type: "input",
+        name: "projectName",
+        message: "What is your devtools project's name?",
+        initial: "my-devtools",
+      },
+      {
+        type: "input",
+        name: "devName",
+        message: "What is your name?",
+        initial: defaultName,
+      },
+      {
+        type: "input",
+        name: "devEmail",
+        message: "What is your email?",
+        initial: defaultEmail,
+      },
+      {
+        type: "input",
+        name: "projectOrg",
+        message: "What will your project's GitHub organization be?",
+      },
+    ])) as any;
 
-  let { projectName } = responses;
-  const projectNameConstant = constantCase(projectName);
-  const projectNameCapital = capitalCase(projectName);
-  projectName = paramCase(projectName);
+    let { projectName } = responses;
+    const projectNameConstant = constantCase(projectName);
+    const projectNameCapital = capitalCase(projectName);
+    projectName = paramCase(projectName);
 
-  responses = {
-    ...responses,
-    projectName,
-    projectNameCapital,
-    projectNameConstant,
-    uuid: `{${uuid()}}`,
-  };
+    responses = {
+      ...responses,
+      projectName,
+      projectNameCapital,
+      projectNameConstant,
+      uuid: `{${uuid()}}`,
+    };
 
-  signale.success("Recorded responses");
-  signale.await(`Loading ${responses.template} template..`);
+    signale.success("Recorded responses");
+    signale.await(`Loading ${responses.template} template..`);
 
-  copy(
-    getTemplatePath(responses.template),
-    `${process.cwd()}/${responses.projectName}/`,
-    responses,
-    (err, createdFiles) => {
-      if (err) throw err;
-      createdFiles.forEach((filePath) =>
-        signale.complete(`Created ${filePath}`)
-      );
-      signale.success(
-        `Navigate into ${responses.projectName}/ to get started!`
-      );
-    }
-  );
+    copy(
+      getTemplatePath(responses.template),
+      `${process.cwd()}/${responses.projectName}/`,
+      responses,
+      (err, createdFiles) => {
+        if (err) throw err;
+        createdFiles.forEach((filePath) =>
+          signale.complete(`Created ${filePath}`)
+        );
+        signale.success(
+          `Navigate into ${responses.projectName}/ to get started!`
+        );
+      }
+    );
+  } catch (e) {
+    signale.complete("Cancelling template creation");
+    console.error(e);
+  }
 };
 
 main();
