ec089fef94334e14623aa0e686e22e3a8cd6c9e8
Tyler Krupicka <tylerkrupicka@gmail.com>
2021-02-02 11:05:19 -0800

    theme arrow

--- components/Tree/src/theme.ts
+++ components/Tree/src/theme.ts
@@ -5,22 +5,26 @@ const TreeTheme = (): ComponentTheme => ({
     light: {
       focusColor: firefox.light.blue03,
       hoverColor: firefox.light.blue01,
+      arrowColor: firefox.light.gray03,
       hoverRadius: "0px",
     },
     dark: {
       focusColor: firefox.dark.blue05,
       hoverColor: firefox.dark.gray04,
+      arrowColor: firefox.dark.gray01,
     },
   },
   chrome: {
     light: {
       focusColor: chrome.light.blue02,
       hoverColor: chrome.light.blue01,
+      arrowColor: chrome.light.gray05,
       hoverRadius: "6px",
     },
     dark: {
       focusColor: chrome.dark.blue04,
       hoverColor: chrome.dark.blue05,
+      arrowColor: chrome.dark.gray02,
     },
   },
 });
